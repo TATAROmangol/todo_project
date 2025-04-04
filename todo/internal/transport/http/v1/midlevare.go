fef95a79dbcae752e8f28454899f213334aca731
TATAROmangol <deadpoolmail.ru@gmail.com>
2025-04-05 03:58:46 +0500

    fix midleware and complete todo_api compsoe

--- todo/internal/transport/http/v1/midlevare.go
+++ todo/internal/transport/http/v1/midlevare.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"context"
 	"net/http"
+	"todo/pkg/jwt"
 	"todo/pkg/logger"
 
 	"github.com/google/uuid"
@@ -31,12 +32,21 @@ func Operation(h func(w http.ResponseWriter, r *http.Request)) func(http.Respons
 
 func Auth(h func(w http.ResponseWriter, r *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// operationId := uuid.NewString()
+		cookie, err := r.Cookie("user_jwt")
+		if err != nil{
+			logger.GetFromCtx(r.Context()).ErrorContext(r.Context(), "not found cookie", "error", err)
+			return
+		}
 
-		// ctx = logger.AppendCtx(ctx, OperationKey, operationId)
-		// logger.GetFromCtx(ctx).InfoContext(ctx, "called method")
+		id, err := jwt.GetId(cookie.Value)
+		if err != nil{
+			logger.GetFromCtx(r.Context()).ErrorContext(r.Context(), "", "error", err)
+			return
+		}
 
-		// r = r.WithContext(ctx)
+		ctx := r.Context()
+		ctx = logger.AppendCtx(ctx, UserIdKey, id)
+		r = r.WithContext(ctx)
 
 		h(w, r)
 	}
